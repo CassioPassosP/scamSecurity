0a37774e9e2ae9e1f2bc6e11cbeac816d7d47542
Cassio Passos <passoscassio29@gmail.com>
2026-07-29 11:44:41 -0300

    troca de local de comentarios

--- src/Chat.java
+++ src/Chat.java
@@ -1,2 +1,11 @@
 public class Chat {
+    //enviarMensagem()
+
+    //receberMensagem()
+
+    //classificarMensagem()
+
+    //Atualiza a classificacao da mensagem
+
+    //emitirAlerta() obs:caso necessario
 }
--- src/Mensagem.java
+++ src/Mensagem.java
@@ -1,10 +1,2 @@
 public class Mensagem {
-
-    //receberMensagem()
-
-    //classificarMensagem()
-
-    //Atualiza a classificacao da mensagem
-
-    //emitirAlerta() obs:caso necessario
 }
